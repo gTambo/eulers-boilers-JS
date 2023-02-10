a6d447224d13ff238ef2d43c9a48c5518359764b
Greg Tambornino <greg.tambornino@concordusa.com>
2023-02-10 12:12:58 -0600

    remove unecessary step and comments

--- app/Problem_1/problem1.js
+++ app/Problem_1/problem1.js
@@ -1,8 +1,7 @@
 function CountMultiplesOf3And5(int) {
     
     const multiples_of_3_and_5 = [];
-    let limit = int;
-    for(let i = 3; i < limit; i++) {
+    for(let i = 3; i < int; i++) {
         let j = i % 3;
         let k =  i % 5;
         if(j === 0 || k === 0){
@@ -14,19 +13,11 @@ function CountMultiplesOf3And5(int) {
     return multiples_of_3_and_5.reduce((a, b) => a + b, 0);
 }
 
-// const startJS = performance.now();
-
-// await CountMultiplesOf3And5(1000);
-
-// const endJS = performance.now();
-// console.log(`Execution time: ${endJS - startJS} ms`);
-
 const start = process.hrtime.bigint()
 await CountMultiplesOf3And5(1000);
 const end = process.hrtime.bigint();
 
 console.log(`Execution time: ${end - start} ns`);
-// console.log(`Execution time: `, Number(end - start)/1000000000,` s`);
 
 console.log("Multiples of 3 and 5", CountMultiplesOf3And5(1000));
 
