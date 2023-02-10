660359bd6cefebb436696e55f64087faef37aaad
Greg Tambornino <greg.tambornino@concordusa.com>
2023-02-09 22:25:12 -0600

    Use remainder instead of remainder assignment assign new variables for comparison

--- app/Problem_1/problem1.js
+++ app/Problem_1/problem1.js
@@ -1,14 +1,18 @@
-export default function CountMultiplesOf3And5() {
-    let multiples_of_3_and_5 = [];
-
-    for(i = 0; i < 1000; i++) {
-        if(i %= 3 === 0){
-            multiples_of_3_and_5.push(i)
-        } else if( i %= 5 === 0){
-            multiples_of_3_and_5.push(i)
+function CountMultiplesOf3And5(int) {
+    
+    const multiples_of_3_and_5 = [];
+    let limit = int;
+    for(let i = 3; i < limit; i++) {
+        let j = i % 3;
+        let k =  i % 5;
+        if(j === 0 || k === 0){
+            multiples_of_3_and_5.push(i);
         }
 
     }
+
     return multiples_of_3_and_5;
 }
-console.log("Multiples of 3 and 5", CountMultiplesOf3And5());
+console.log("Multiples of 3 and 5", CountMultiplesOf3And5(1000));
+
+export default CountMultiplesOf3And5;
