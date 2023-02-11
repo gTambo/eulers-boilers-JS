13620c1a1ab7fa11f5bd811f19fa4dc4ec72833e
Greg Tambornino <greg.tambornino@concordusa.com>
2023-02-10 18:30:18 -0600

    complete function to find possible solution, pre-cleanup

--- app/Problem_3/problem3.js
+++ app/Problem_3/problem3.js
@@ -1,12 +1,10 @@
 function checkPrime(int) {
-    console.log(`Checking if ${int} is prime`);
     if(int < 2 ) {
         return false;
     } else if (int === 2) {
         return true;
     } else {
         for(let i = 2; i < int; i++) {
-            // console.log("i is: ", i);
             if(int % i === 0) {
                 return false;
             }
@@ -15,29 +13,75 @@ function checkPrime(int) {
     }
 }
 
-function generateIntegerArray(length) {
-    const arr = [];
-    for(let i = 0; i < length; i++) {
-        arr.push(i + 1);
+// function generateIntegerArray(length) {
+//     const arr = [];
+//     for(let i = 0; i < length; i++) {
+//         arr.push(i + 1);
+//     }
+//     return arr;
+// }
+
+function findHighestPrimeFactor(int){
+    // const factors = [];
+    // const possibleFactors = generateIntegerArray(int);
+    let start = 2;
+    let end = Math.floor(int / 2);
+    while(start <= end){
+        if(int % end === 0) {
+            // factors.push(end);
+            if(checkPrime(end)){
+                return end;
+            }
+        } 
+        if (int % start === 0) {
+            // factors.push(start);
+            if(checkPrime(start)){
+                return start;
+            }
+        }
+        start++;
+        end--;
     }
-    return arr;
+    return "none found";
+}
+
+function returnPrime(int) {
+    let divisorLess = Math.floor(int / 2);
+    let divisorMore = Math.ceil(int / 2);
+    console.log("divisorLess: ", divisorLess, 'divisorMore: ', divisorMore);
+    console.log('int: ', int);
+
+    if(divisorLess === divisorMore){
+        return false;
+    }
+    if(divisorLess === 1){
+        return true;
+    }
+    // if(int % divisor === 0){
+    //     return false;
+    // }
+
+    while(int % divisorLess > 0){
+        return returnPrime(divisorMore);
+    }
 }
 
 const countTo20 = generateIntegerArray(20);
 const countTo100 = generateIntegerArray(100);
 
-function testPrimeChecker(arr) {
-    const primes = [];
-    for(let i = 0; i < arr.length; i++) {
-        if(checkPrime(arr[i])){
-            primes.push(arr[i]);
-        }
-    }
-    return primes;
-}
-
-// let check = checkPrime(16);
-console.log("checking primes up to 100...", testPrimeChecker(countTo100));
-// console.log("testing checkPrime function... 1", check);
+// function testPrimeChecker(arr) {
+//     const primes = [];
+//     for(let i = 0; i < arr.length; i++) {
+//         if(checkPrime(arr[i])){
+//             primes.push(arr[i]);
+//         }
+//     }
+//     return primes;
+// }
+console.log('check highest prime factor of 600851475143...', findHighestPrimeFactor(600851475143));
+console.log(checkPrime(71));
+// console.log("hopefully not infinite...", returnPrime(101));
+// console.log("checking primes up to 100...", testPrimeChecker(countTo100));
+// console.log('looking for factors of 13195: ', findFactors(13195).filter(x => checkPrime(x)));
 
 export default checkPrime;
